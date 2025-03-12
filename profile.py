81b3c27a6afaf6967fbb5d6d4c9249d9c3c49a0d
Udara Wijesinghe <103722292+Udaramalinda@users.noreply.github.com>
2025-03-12 11:55:49 +0530

    Update profile.py

--- profile.py
+++ profile.py
@@ -5,18 +5,10 @@ import geni.rspec.pg as rspec
 # Create a request object
 request = portal.Context().makeRequestRSpec()
 
-# Define node 1
-node1 = request.RawPC("node1")
-node1.hardware_type = "c8220"  # Use c8220 machine
-node1.disk_image = "urn:publicid:IDN+emulab.net+image+Ubuntu20-04"
-
-# Define node 2
-node2 = request.RawPC("node2")
-node2.hardware_type = "c8220"  # Use c8220 machine
-node2.disk_image = "urn:publicid:IDN+emulab.net+image+Ubuntu20-04"
-
-# Create a network link between the two nodes
-link = request.Link(members=[node1, node2])
+# Define a single node
+node = request.RawPC("node")
+node.hardware_type = "c8220"  # Use c8220 machine
+node.disk_image = "urn:publicid:IDN+emulab.net+image+Ubuntu20-04"
 
 # Print the final RSpec
 portal.Context().printRequestRSpec()
